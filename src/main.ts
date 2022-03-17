f6cdbaf15f40cbfdffe7863c82a1e4a94b525806
olcay <olcayusta02@gmail.com>
2022-03-17 22:34:39 +0300

    Experimental RenderComponent Test

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, ɵrenderComponent as renderComponent } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import {AppComponent} from "./app/app.component";
 
 if (environment.production) {
   enableProdMode();
 }
 
+renderComponent(AppComponent)
+
+/*
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZone: 'noop'
 })
   .catch(err => console.error(err));
+*/
